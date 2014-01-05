8709d746314db5c1abbe02e034bd2c7162dcbc19
liz rush <liz@lizmrush.com>
2014-01-05 14:08:59 -0800

    adds pg gem for heroku

--- app/controllers/mcsubscribe_controller.rb
+++ app/controllers/mcsubscribe_controller.rb
@@ -13,34 +13,34 @@ class McsubscribeController < ApplicationController
         @mc.lists.subscribe(@list_id, {'email' => email})
          
         respond_to do |format|  
-          format.json{render :json => {:message => "Success! Check your email to confirm sign up."}}
+          format.json{render :json => {:message => "Success! Check your email to confirm."}}
         end
        
         rescue Mailchimp::ListAlreadySubscribedError
           respond_to do |format| 
-            format.json{render :json => {:message => "#{email} is already subscribed to the list"}}
+            format.json{render :json => {:message => "Looks like #{email} is already subscribed to the list"}}
           end
          
         rescue Mailchimp::ListDoesNotExistError
           respond_to do |format|  
-            format.json{render :json => {:message => "The list could not be found."}}
+            format.json{render :json => {:message => "Whoops, the list could not be found."}}
           end
          
         rescue Mailchimp::Error => ex
             if ex.message
               respond_to do |format|        
-                format.json{render :json => {:message => "There is an error. Please enter valid email id."}}
+                format.json{render :json => {:message => "Please enter a valid email address."}}
               end
             else
               respond_to do |format|        
-                format.json{render :json => {:message => "An unknown error occurred."}}
+                format.json{render :json => {:message => "An unknown error occurred. Try again later."}}
               end              
             end   
         end
          
         else
           respond_to do |format|        
-            format.json{render :json => {:message => "Email Address Cannot be blank. Please enter valid email id."}}
+            format.json{render :json => {:message => "Looks like you didn't actually input anything... please try again!"}}
           end
         end
   end
