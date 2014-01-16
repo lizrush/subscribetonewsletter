f548c7bd1017c5a47a17c2334dd454db45a77152
liz rush <liz@lizmrush.com>
2014-01-16 13:51:05 -0800

    all tables and variables on erb

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,8 +1,10 @@
 class VisitorsController < ApplicationController
   protect_from_forgery with: :exception
   before_action :update_pageviews_count, only: [:index]
+  helper_method :count_visitors, :count_browsers, :count_mobile
 
   def index
+    puts "index before variables"
     @total_visitors = count_visitors[:total]
     @returning_visitors = count_visitors[:returning]
     @new_visitors = count_visitors[:newvisitors]
@@ -13,28 +15,32 @@ class VisitorsController < ApplicationController
     @other_browsers = count_browsers[:other]
 
     @mobile = count_mobile
-
-    
   end
 
-private
+  private 
 
+ # refactor this later into model using sql to group and count
   def count_visitors
-    count_visitors[:total] = Visitors.all.count
-    count_visitors[:returning] = Visitors.where(:numberofvisits > 1).count
-    count_visitors[:new] = Visitors.where(:numberofvisits < 2).count
+    count_visitors = {}
+    count_visitors[:total] = Visitor.all.count
+    count_visitors[:returning] = Visitor.where('numberofvisits'.to_i > 1).count
+    count_visitors[:new] = Visitor.where('numberofvisits'.to_i < 2).count
+    count_visitors
   end
 
   def count_browsers
-    count_browsers[:chrome] = Visitors.where(browser: "Chrome").count
-    count_browsers[:safari] = Visitors.where(browser: "Safari").count
-    count_browsers[:firefox] = Visitors.where(browser: "Firefox").count
-    main_browsers = count_browsers[:chrome] + count_browsers[:safari] + count_browsers[:firefox]
-    count_browsers[:other] = Visitors.all.count - main_browsers
+    count_browsers = {}
+    count_browsers[:chrome] = Visitor.where(browser: "Chrome").count
+    count_browsers[:safari] = Visitor.where(browser: "Safari").count
+    count_browsers[:firefox] = Visitor.where(browser: "Firefox").count
+    count_browsers[:ie] = Visitor.where(browser: "Internet Explorer").count
+    main_browsers = count_browsers[:chrome] + count_browsers[:safari] + count_browsers[:firefox] + count_browsers[:ie]
+    count_browsers[:other] = Visitor.all.count - main_browsers
+    count_browsers
   end
 
   def count_mobile
-    Visitors.where(mobile: true).count
+    Visitor.where(mobile: 'true').count
   end
 
 end
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -2,25 +2,5 @@ class Visitor < ActiveRecord::Base
  validates :ip_address,    presence: true
 
 
-  
-  def self.return_visitors
-    where('pageviews > 1')
-  end
-
-
-  def self.mobile_users
-    where(mobile: true)
-  end
- 
-
-  def self.non_mobile_users
-    where(mobile: false)
-  end
-
-  def self.total_views
-    sum(:ip_address)
-  end
-
-
 
 end
