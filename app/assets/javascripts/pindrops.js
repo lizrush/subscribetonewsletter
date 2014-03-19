2aa692c6c59c54e928262aa9e0d1ce1db5b520c1
Liz Rush <liz@lizmrush.com>
2014-03-18 19:56:16 -0700

    Update pindrops.js

--- app/assets/javascripts/pindrops.js
+++ app/assets/javascripts/pindrops.js
@@ -1,15 +1,14 @@
 function initialize() {
 
-
 var myOptions =  {
    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: new google.maps.LatLng(30, -10),
-    zoom: 2,
+     center: new google.maps.LatLng(30, -10),
+     zoom: 2,
    disableDefaultUI: true
   };
+  
 var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 var lat_long_array = gon.lat_long_array;
-
 
 for (var i = 0; i < lat_long_array.length; i++) {
   var lat_lngs = new google.maps.LatLng(lat_long_array[i][0], lat_long_array[i][1]);
